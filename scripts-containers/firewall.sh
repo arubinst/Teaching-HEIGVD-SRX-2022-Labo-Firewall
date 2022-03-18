34a35a2ac38abc63bb3df4d7f1c108419873d703
Damien Maier <91182145+damienmaier@users.noreply.github.com>
2022-03-18 03:51:57 -0700

    règles ping

--- scripts-containers/firewall.sh
+++ scripts-containers/firewall.sh
@@ -11,6 +11,19 @@ service ssh start
 nft add table firewall
 nft 'add chain firewall forward {type filter hook forward priority 0 ; policy drop ; }'
 
+# règles ping
+nft add rule firewall forward \
+ip saddr 192.168.100.0/24 icmp type echo-request accept \
+comment \"autorise le LAN à tout pinger\"
+
+nft add rule firewall forward \
+ip saddr 192.168.200.0/24 ip daddr 192.168.100.0/24 icmp type echo-request accept \
+comment \"autorise la DMZ à pinger le LAN\"
+
+nft add rule firewall forward \
+ct state established icmp type echo-reply accept \
+comment \"on autorise toutes les réponses à des requêtes de ping autorisées\"
+
 
 # on affiche la configuration obtenue
 nft list ruleset
