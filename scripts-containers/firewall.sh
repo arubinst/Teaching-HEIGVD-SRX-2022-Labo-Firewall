1bf27ca6caa440da93cd03c0ce93b47d220829c7
Damien Maier <91182145+damienmaier@users.noreply.github.com>
2022-03-18 02:58:27 -0700

    correction

--- scripts-containers/firewall.sh
+++ scripts-containers/firewall.sh
@@ -9,7 +9,7 @@ service ssh start
 
 # création de la table et de la chaîne pour les règles de filtrage du firewall
 nft add table firewall
-nft 'add chain firewall forward {type filter hook forward priority ; policy drop ; }'
+nft 'add chain firewall forward {type filter hook forward priority 0 ; policy drop ; }'
 
 
 # on affiche la configuration obtenue
