016b8655405e737ff324842a26029a98e710d382
Luhrel <57545107+Luhrel@users.noreply.github.com>
2022-03-18 11:36:31 +0100

    Detect terminal and add  Konsole to run-infra.sh

--- run-infra.sh
+++ run-infra.sh
@@ -1,4 +1,23 @@
+#!/bin/bash
+
 docker-compose up -d
-gnome-terminal --tab -- "scripts/openfirewall.sh"
-gnome-terminal --tab -- "scripts/opendmz.sh"
-gnome-terminal --tab -- "scripts/openlan.sh"
+
+SCRIPTS=("scripts/openfirewall.sh" "scripts/opendmz.sh" "scripts/openlan.sh")
+
+if command -v gnome-terminal &> /dev/null
+then
+    CMD="gnome-terminal --tab --"
+elif command -v konsole &> /dev/null
+then
+    CMD="konsole --new-tab -e"
+fi
+
+
+if [[ ! -z $CMD ]]
+then
+    for i in "${SCRIPTS[@]}"
+    do
+        eval "${CMD} ./${i}" &
+    done
+fi
+
