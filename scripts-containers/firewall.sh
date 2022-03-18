0387b599b15b0059a370b7bd68ff6fda85196ca6
Damien Maier <91182145+damienmaier@users.noreply.github.com>
2022-03-18 02:56:50 -0700

    créé la chaîne de filtre du firewall

--- scripts-containers/firewall.sh
+++ scripts-containers/firewall.sh
@@ -7,6 +7,10 @@ nft 'add chain nat postrouting { type nat hook postrouting priority 100 ; }'
 nft add rule nat postrouting meta oifname "eth0" masquerade
 service ssh start
 
+# création de la table et de la chaîne pour les règles de filtrage du firewall
+nft add table firewall
+nft 'add chain firewall forward {type filter hook forward priority ; policy drop ; }'
+
 
 # on affiche la configuration obtenue
 nft list ruleset
