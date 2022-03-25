e724486151f4bd31834dd6358f51fcfcd936088a
Damien Maier <91182145+damienmaier@users.noreply.github.com>
2022-03-25 03:28:42 -0700

    règles web

--- scripts-containers/firewall.sh
+++ scripts-containers/firewall.sh
@@ -11,7 +11,10 @@ service ssh start
 nft add table firewall
 nft 'add chain firewall forward {type filter hook forward priority 0 ; policy drop ; }'
 
-# règles ping
+#
+# règles pour le ping
+#
+
 nft add rule firewall forward \
 ip saddr 192.168.100.0/24 icmp type echo-request accept \
 comment \"autorise le LAN à tout pinger\"
@@ -33,6 +36,31 @@ nft add rule firewall forward \
 ip saddr 192.168.100.0/24 tcp dport 53 accept \
 comment \"autorise le LAN à envoyer des requêtes DNS \(TCP\) sur le WAN\"
 
+#
+# Règles pour HTTP et HTTPS du LAN vers WAN
+#
+
+nft add rule firewall forward \
+ip saddr 192.168.100.0/24 ip daddr !=192.168.0.0/16 tcp dport 80 accept \
+comment \"autorise le LAN à ouvrir des connexions TCP vers le WAN sur le port 80\"
+
+nft add rule firewall forward \
+ip saddr 192.168.100.0/24 ip daddr !=192.168.0.0/16 tcp dport 8080 accept \
+comment \"autorise le LAN à ouvrir des connexions TCP vers le WAN sur le port 8080\"
+
+
+nft add rule firewall forward \
+ip saddr 192.168.100.0/24 ip daddr !=192.168.0.0/16 tcp dport 443 accept \
+comment \"autorise le LAN à ouvrir des connexions TCP vers le WAN sur le port 443\"
+
+
+#
+# Règles pour HTTP vers la DMZ
+#
+
+nft add rule firewall forward \
+ip daddr 192.168.200.3 tcp dport 80 accept \
+comment \"autorise tout le monde à ouvrir des connexions TCP vers le serveur web de la DMZ sur le port 80\"
 
 
 
@@ -43,4 +71,3 @@ comment \"on autorise toutes les réponses à des requêtes que nous avons autor
 
 # on affiche la configuration obtenue
 nft list ruleset
-
